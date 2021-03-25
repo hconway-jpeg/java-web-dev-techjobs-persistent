37259ae6b0e573350e0756312d0bc287ac667036
unknown <hconway@luc.edu>
2021-03-25 18:42:58 -0500

    part 2 - models - complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 125)
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 300)
+    private String description;
+
+    public Skill() {}
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 }
