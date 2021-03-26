9228e33b3a46af740a22cbba67f141ab66288fb4
unknown <hconway@luc.edu>
2021-03-26 10:40:00 -0500

    part 3 - add jobs Field to Employer - complete; not sure if totally correct

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,9 +1,14 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +16,11 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size(max = 125, message = "Must not be longer than 125 characters")
     private String location;
+
+    // cascading allows for employer updates to be sent to the employers jobs as well??
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employer.id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
